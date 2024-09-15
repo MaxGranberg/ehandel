c3cfec5ed069af381d5ea460c41f74e22914f230
Max Granberg <mg224em@student.lnu.se>
2024-09-15 13:11:14 +0200

    Add a flash message after adding product to cart

--- src/components/addToCartButton.tsx
+++ src/components/addToCartButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useCart } from "@/context/cartContext";
+import { useState } from "react";
 
 type AddToCartButtonProps = {
   product: {
@@ -12,14 +13,27 @@ type AddToCartButtonProps = {
 
 export default function AddToCartButton( { product }: AddToCartButtonProps) {
   const { dispatch } = useCart();
+  const [ showMessage, setShowMessage ] = useState(false);
 
   const addToCart = () => {
     dispatch({ type: "ADD_TO_CART", product });
+
+    //Confirmation message after adding a product to cart, hide after 3 seconds
+    setShowMessage(true)
+    setTimeout(() => {
+      setShowMessage(false)
+    }, 3000)
   };
 
   return (
+    <div>
     <button onClick={addToCart} className="bg-blue-500 text-white px-4 py-2 rounded"> 
       Buy Now
     </button>
+
+    {showMessage && (
+      <p className="mt-2 text-green-500">Product added to cart!</p>
+    )}
+    </div>
   );
 }
